26b0e0f50e2e8a36843f3780a2acbe271a2d9db3
rohit-kumar-j <jrohit1110@gmail.com>
2025-04-28 19:17:08 -0400

    Pehala Commit

---
+++ submission.py
@@ -0,0 +1,19 @@
+"""
+See example.py for examples
+Implement Controller class with your motion planner
+"""
+
+class Controller:
+    def __init__(self):
+        # load any optimized model data here
+        pass
+
+    def run(self, env, goal_poses):
+        # run the controller in the environment to achieve the goal
+        pass
+
+if __name__ == "__main__":
+
+    # you can edit this part for informal testing
+    pass
+
